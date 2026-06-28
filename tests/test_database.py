7f7148326b3a6f51bab024928ef89632e849ca5f
damianbugajDE <damianbugajit@gmail.com>
2026-06-28 12:48:31 +0200

    :wrench: Added test_database test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,14 @@
 import os
-import pytest
+
 import duckdb
-from loader.load_bronze import DB_PATH, load_tables 
+import pytest
+
+from loader.load_bronze import DB_PATH, load_tables
+
 
 def test_customers_table_has_data():
     """Test checking, if table raw_customers has records after load."""
-    #check if file of database doesn't exists before test
+    # check if file of database doesn't exists before test
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
 
@@ -28,9 +31,10 @@ def test_customers_table_has_data():
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
 
-def test_database_file_created()
-    """ Check if file of database exists after the operation is done"""
-    open(DB_PATH, 'a').close()
+
+def test_database_file_created():
+    """Check if file of database exists after the operation is done"""
+    open(DB_PATH, "a").close()
 
     assert os.path.exists(DB_PATH) == True
 
